58c849e5c71213736c66a7fb8da487d8e3347068
vsdonline <viswasaidwarakami@gmail.com>
2016-05-02 22:54:37 -0400

    minor defect fixes

--- VSDOnline/Scripts/histats-counter.js
+++ VSDOnline/Scripts/histats-counter.js
@@ -1,4 +1,16 @@
-﻿var _Hasync = _Hasync || [];
+﻿//var _Hasync = _Hasync || [];
+//_Hasync.push(['Histats.start', '1,3431621,4,403,118,80,00011111']);
+//_Hasync.push(['Histats.fasi', '1']);
+//_Hasync.push(['Histats.track_hits', '']);
+//(function () {
+//    var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+//    hs.src = ('http://s10.histats.com/js15_as.js');
+//    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+//    document.getElementById('histats_counter').addEventListener("click", function (e) {
+//        if (e.target.tagName == "CANVAS") e.preventDefault();
+//    });
+//})();
+var _Hasync = _Hasync || [];
 _Hasync.push(['Histats.start', '1,3431621,4,403,118,80,00011111']);
 _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
@@ -6,7 +18,7 @@ _Hasync.push(['Histats.track_hits', '']);
     var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
     hs.src = ('http://s10.histats.com/js15_as.js');
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-    document.getElementById('histats_counter').addEventListener("click", function (e) {
-        if (e.target.tagName == "CANVAS") e.preventDefault();
-    });
+        document.getElementById('histats_counter').addEventListener("click", function (e) {
+            if (e.target.tagName == "CANVAS") e.preventDefault();
+        });
 })();
