51827e501e4c1c5c1a2f0071fbe69ee826548806
vsdonline <viswasaidwarakami@gmail.com>
2016-05-03 18:18:25 -0400

    Recreated histats

--- VSDOnline/Scripts/histats-counter.js
+++ VSDOnline/Scripts/histats-counter.js
@@ -11,14 +11,14 @@
 //    });
 //})();
 var _Hasync = _Hasync || [];
-_Hasync.push(['Histats.start', '1,3431621,4,403,118,80,00011111']);
+_Hasync.push(['Histats.start', '1,3442349,4,403,118,80,00011111']);
 _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
 (function () {
     var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
     hs.src = ('http://s10.histats.com/js15_as.js');
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-        document.getElementById('histats_counter').addEventListener("click", function (e) {
-            if (e.target.tagName == "CANVAS") e.preventDefault();
-        });
+    document.getElementById('histats_counter').addEventListener("click", function (e) {
+        if (e.target.tagName == "CANVAS") e.preventDefault();
+    });
 })();
