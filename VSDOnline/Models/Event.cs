bf463de92e9c45782dd7b14b482ff29ecd918500
viswasaidwarakamai <viswasaidwarakamai@gmail.com>
2016-10-15 02:01:06 -0400

    minor changes

--- VSDOnline/Models/Event.cs
+++ VSDOnline/Models/Event.cs
@@ -3,17 +3,30 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace VSDOnline.Models
 {
     public class Event
     {
         public int ID { get; set; }
+
+        [AllowHtml]
         public string Name { get; set; }
+
+        [AllowHtml]
         public string Description { get; set; }
+
+        [AllowHtml]
         public string Date { get; set; }
+
+        [AllowHtml]
         public string Time { get; set; }
+
+        [AllowHtml]
         public string Venue { get; set; }
+
+        [AllowHtml]
         public string ContactDetails { get; set; }
     }
 }
